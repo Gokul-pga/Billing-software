2242f78f99087a7d6c931e300630c55bbc24432d
Gokul-pga <gokulanand2508@gmail.com>
2024-08-07 15:26:11 +0530

    cashflow added

--- src/screens/settings/settings.tsx
+++ src/screens/settings/settings.tsx
@@ -1,38 +1,36 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useState} from 'react';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
-import {ToggleSwitch, TopHeader} from '../../components/commonComponents';
-import {settingsData} from '../../utils/data/settingsData';
+import React, {useEffect, useState} from 'react';
+import {Image, Text, View} from 'react-native';
 import CustomIcon from '../../utils/icons';
-import {labels} from '../../utils/labels';
-import {ProfileImage} from '../../utils/png';
-import {screenName} from '../../utils/screenNames';
-import {
-  BottomWidth,
-  H15blackOne500,
-  H18BlackOne700,
-} from '../../utils/styledComponents';
-import {colors} from '../../utils/theme/colors';
-import {
-  alignItemCenter,
-  alignSelfCenter,
-  bg_color_white,
-  flex1,
-  flexRow,
-  justifyBetween,
-  justifyCenter,
-  mh10,
-  mh15,
-  mv10,
-  mv15,
-  mv5,
-} from '../../utils/theme/commonStyles';
+import axios from 'axios';
+import { api } from '../../../envfile/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+type User =[]
 
 export type SettingsProps = {};
 
 const SettingsScreen = (props: SettingsProps) => {
-  const [toggleVisible, setToggleVisible] = useState(false);
   const navigation = useNavigation();
+
+  const [addUser, setaddUser] = useState<User[]>()
+
+useEffect(() => {
+    const email =  AsyncStorage.getItem('userEmail');
+    fetchProducts();
+}, [])
+
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(api+
+        '/auth/getUser',
+      );
+      setaddUser(response.data.data); // Assuming response.data.data is an array of products
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
 
   return (
     <View
